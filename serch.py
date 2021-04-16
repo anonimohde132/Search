14d0290d139bbc2875e54abb8fd3e079aade7989
anonimohde132 <56239775+anonimohde132@users.noreply.github.com>
2021-04-16 15:12:17 -0300

    search 1.0

---
+++ serch.py
@@ -0,0 +1,25 @@
+import os
+
+print("""
++====================================+
+           ______________
+          | HDE Products |
+          |______________|
+          
+          HeHeHeHeHeHeHe
++====================================+
+
+{1} Instalar ferramentas (Para kali linux apenas)
+{2} Info Do site completa
+
+
+""")
+
+
+ask = int(input(' {Escolha} {> '))
+
+if ask == '1':
+  os.system('apt install parsero -y && apt install nikto -y && apt install dnsenum -y')
+elif ask == '2':
+ask2 == str(input(' {Site} ==>> '))
+os.system('whois ' + ask2)
