2154847f3fb947ea16e86ca9993f35a7719f1b3d
anonimohde132 <56239775+anonimohde132@users.noreply.github.com>
2021-04-16 15:14:52 -0300

    Update serch.py

--- serch.py
+++ serch.py
@@ -21,5 +21,5 @@ ask = int(input(' {Escolha} {> '))
 if ask == '1':
   os.system('apt install parsero -y && apt install nikto -y && apt install dnsenum -y')
 elif ask == '2':
-ask2 == str(input(' {Site} ==>> '))
-os.system('whois ' + (ask2))
+           ask2 == str(input(' {Site} ==>> '))
+           os.system('whois ' + ask2)
